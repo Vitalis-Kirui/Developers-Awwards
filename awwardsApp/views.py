255a388a5d6f9827a7c82f9df38c39afcbe8beaa
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-23 22:48:11 +0300

    pushing API VIEW class on  views.py

--- awwardsApp/views.py
+++ awwardsApp/views.py
@@ -9,6 +9,13 @@ from django.contrib.auth import login, authenticate
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 
+#APIS
+from .models import Profile, Project, Rates
+from .serializers import ProfileSerializer, ProjectSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 def index(request):
@@ -122,3 +129,24 @@ def view_project(request, id):
 
     }
     return render(request, 'view-project.html', params)
+
+class ProfileList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+
+    def get(self, request, format=None):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+class ProjectList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+
+    def get(self, request, format=None):
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
