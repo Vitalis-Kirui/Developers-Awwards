9fb47a5b5434db1a89643a64e10cc236d8a8273d
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-23 11:15:14 +0300

    pushing project class model

--- awwardsApp/models.py
+++ awwardsApp/models.py
@@ -20,3 +20,30 @@ class Profile(models.Model):
 
     def update_profile(cls, id):
         Project.objects.get(user_id=id)
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=150)
+    image = CloudinaryField('images')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    url = models.URLField(blank=True)
+    description = models.TextField(max_length=300, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        return self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def project_by_id(cls, id):
+        project = Project.objects.filter(id=id)
+        return project
+
+    @classmethod
+    def search_project(cls, name):
+        return cls.objects.filter(title__icontains=name).all()
