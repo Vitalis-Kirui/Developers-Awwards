aed7007f340a4527775a2010fd789863a1875bbb
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-23 11:34:45 +0300

    pushing search view function

--- awwardsApp/urls.py
+++ awwardsApp/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
+    path('search/', views.search, name='search'),
 ]
--- awwardsApp/views.py
+++ awwardsApp/views.py
@@ -7,3 +7,13 @@ def index(request):
     profile = Profile.objects.all()
     projects = Project.objects.all()
     return render(request, 'index.html', {"profile": profile, "projects": projects})
+
+def search(request):
+    if 'project' in request.GET and request.GET['project']:
+        project = request.GET.get("project")
+        results = Project.search_project(project)
+        message = f'project'
+        return render(request, 'search.html', {'projects': results, 'message': message})
+    else:
+        message = "You haven't searched for anything, please try again"
+    return render(request, 'search.html', {'message': message})
