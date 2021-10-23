4e07ca5da58ba4e488600ff95e4d134d330dc484
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-23 11:31:01 +0300

    updating index.html file for display models data

--- awwards/urls.py
+++ awwards/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^awwards/',include('awwardsApp.urls'))
+    url(r'',include('awwardsApp.urls'))
 ]
--- awwardsApp/views.py
+++ awwardsApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Profile, Project, Rates
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', )
+    profile = Profile.objects.all()
+    projects = Project.objects.all()
+    return render(request, 'index.html', {"profile": profile, "projects": projects})
