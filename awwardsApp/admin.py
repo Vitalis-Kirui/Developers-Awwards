9f90c0a4e41fa9ab3bcf1b1d79137b49a73880bc
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-23 11:26:13 +0300

    registering models to admin page

--- awwardsApp/admin.py
+++ awwardsApp/admin.py
@@ -1,3 +1,7 @@
+from awwardsApp.models import Profile, Project, Rates
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Rates)
